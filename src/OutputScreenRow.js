fc1554fb9cb302d240aefe62a4d61b498f58ea63
kssingh0015 <56338695+kssingh0015@users.noreply.github.com>
2021-06-17 09:40:46 +0530

    Calculator-starter

---
+++ src/OutputScreenRow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OutputScreenRow = () => {
+    return(
+        <div>
+            <input type="text" />
+        </div>
+    )
+}
+
+export default OutputScreenRow;
